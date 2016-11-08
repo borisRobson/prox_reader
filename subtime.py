02b2db4dcc0a9b6d41d0c686a48d601013e9a9d8
Brandon <brobsonc@yahoo.co.uk>
2016-11-08 11:04:23 +0000

    calculates time diff

--- subtime.py
+++ subtime.py
@@ -17,10 +17,14 @@ def check_data():
 	missed = 0
 	while i < (len(data) -1):
 		val = json.loads(data[i])
-		msgtime = val['time']
-		rectime = rectimes[i]
+		fullmsgtime = str(val['time'])
+		msgtime = fullmsgtime[6:]
+		fullrectime = str(rectimes[i])
+		rectime = fullrectime[6:]
+		diff = float(rectime) - float(msgtime)
 		id = val['id']		
 		print("msgId: " + str(id) +  ",msgtime : " + str(msgtime)+ ",rectime: " + str(rectime))
+		print("diff: " + str(diff))
 		i += 1
 	mqttc.loop_stop()
 		
@@ -46,7 +50,6 @@ mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 
 mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
-#global msg_count
 mqttc.loop_forever()
 
 mqttc.disconnect()
